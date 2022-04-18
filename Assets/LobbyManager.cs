feb92d2b7db3a689b1548b2fe409f0a74ab2479d
Tyler Kaplan <tmkaplan94@gmail.com>
2022-04-17 19:32:45 -0700

    Added Lobby scene, connects to random room

---
+++ Assets/LobbyManager.cs
@@ -0,0 +1,106 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    #region Private Serialized Fields
+
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players " +
+             "and a new room will be created.")]
+    [SerializeField] private byte maxPlayersPerRoom = 2;
+
+    [Tooltip("The UI Panel to let the user enter name, connect and play")]
+    [SerializeField] private GameObject launcherPanel;
+
+    [Tooltip("The UI Label to inform the user that the connection is in progress")]
+    [SerializeField] private GameObject connectingText;
+
+    #endregion
+
+
+    #region Private Fields
+
+    // this client's version number
+    private string _gameVersion = "1";
+
+    #endregion
+
+
+    #region MonoBehavior Callbacks
+    
+    private void Awake()
+    {
+        // all clients in the same room will automatically sync level
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    private void Start()
+    {
+        // sets the proper display panel/label
+        connectingText.SetActive(false);
+        launcherPanel.SetActive(true);
+    }
+
+    #endregion
+
+
+    #region MonoBahaviorPunCallbacks Callbacks
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() was called by PUN");
+
+        // attempt to join an existing room. if not, OnJoinRandomFailed() will be called
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
+
+        // sets the proper display panel/label
+        connectingText.SetActive(false);
+        launcherPanel.SetActive(true);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRandomFailed() was called by PUN.");
+
+        // failed to join a random room, so create a new room
+        Debug.Log("Calling PhotonNetwork.CreateRoom()");
+        PhotonNetwork.CreateRoom(null, new RoomOptions() {MaxPlayers = maxPlayersPerRoom});
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room.");
+    }
+
+    #endregion
+
+
+    #region Public Methods
+
+    // start the connection process
+    public void Connect()
+    {
+        // sets the proper display panel/label
+        connectingText.SetActive(true);
+        launcherPanel.SetActive(false);
+
+        // join a random room if connected to server, otherwise connect to server
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = _gameVersion;
+        }
+    }
+
+    #endregion
+}
--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Launcher : MonoBehaviour
+public class TitleManager : MonoBehaviour
 {
 
     [SerializeField] private string localScene;
